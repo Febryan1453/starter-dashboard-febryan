f794e32ad828c83fb175380bb307e188c356fb3e
Febryan1453 <brayenfebryan@gmail.com>
2024-11-15 23:55:32 +0700

    Starter Dashbord Febryan

---
+++ resources/js/Pages/My/Dashboard/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyLayout from '../../../Layouts/MyLayout'
+import { Head } from '@inertiajs/inertia-react'
+
+export default function Index() {
+  return (
+    <MyLayout>
+        <Head title="Dashboard" />
+        Halo, Febryan
+    </MyLayout>
+  )
+}
